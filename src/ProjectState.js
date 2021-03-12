28cfbfad85a69dd422d0ba63b87e3faf6e5e8961
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-03-12 12:28:53 -0800

    added images to img folder

---
+++ src/ProjectState.js
@@ -0,0 +1,13 @@
+const ProjectState = () => {
+  return [
+    {
+      title: "Pokedex",
+      description: "This is a description of my Pokedex.",
+      img: null,
+      project: url,
+      github: url,
+    },
+  ];
+};
+
+export default ProjectState;
